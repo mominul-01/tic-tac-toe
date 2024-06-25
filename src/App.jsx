c1bfa27ce834575fa137f6d0cdd8066d484d6dbb
mominul-01 <mominul.cse21@gmail.com>
2024-06-25 15:07:24 +0600

    Project create done with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <div>dfdfd</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
